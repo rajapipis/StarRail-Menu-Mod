7119a2ebbd7b0eaa527853886bc6d89f20edac66
rajapipis <133135016+rajapipis@users.noreply.github.com>
2023-05-13 23:17:49 +0700

    Create main.cpp

---
+++ injector/main.cpp
@@ -0,0 +1,8 @@
+#include <Windows.h>
+#include <iostream>
+#include <filesystem>
+#include <optional>
+#include <fstream>
+#include <commdlg.h>
+#define _CRT_SECURE_NO_WARNINGS
+namespace fs = std::filesystem;
